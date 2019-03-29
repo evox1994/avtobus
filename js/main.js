b557a99a2bb16edfab073b5cff0b038855ac003d
evox1994 <dmitrybatukhtin@gmail.com>
2019-03-29 16:26:33 +0700

    sdfvdfvdfv

--- js/main.js
+++ js/main.js
@@ -200,4 +200,12 @@ $(document).ready(function(){
 		}
 	});
 
+	$('body').on('click','.scroll-btn',function(){
+		var el = $(this).attr('href');
+		var des = $(el).offset().top - 100;
+
+		$('body,html').animate({scrollTop: des},800);
+		return false;
+	});
+
 });
